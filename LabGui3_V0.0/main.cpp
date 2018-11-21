89d2067aa7f7731e5ef578f87f140ec3c830fb12
royrojas07 <38670379+royrojas07@users.noreply.github.com>
2018-11-21 13:56:55 -0800

    last

--- LabGui3_V0.0/main.cpp
+++ LabGui3_V0.0/main.cpp
@@ -21,87 +21,80 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    Grafo g( 10, 0.9 );
-    cout << "El estado es: " << g[9] << endl;
-    Simulador sim(g);
-    sim.iniciarSimulacion(8, 0.6, 3, 0.3, 0.2);
-    cout << "El estado es: " << g[9] << endl;
-    sim.simular();
-    cout << "El estado es: " << g[9] << endl;
-//    cout << "Bienvenido al simulador de dispersión de virus." << endl;
-//    cout << "Ingrese el nombre del archivo de experimentos: ";
-//    string nombreExperimentos;
-//    cin >> nombreExperimentos;
-//    cout << endl;
-//    ifstream archivoExperimentos( nombreExperimentos.c_str() );
-//    if( archivoExperimentos.fail() ){
-//        cout << "El archivo " << nombreExperimentos << " de extensión .txt no existe.";
-//        return 0;
-//    }
-//    
-//    Grafo grafo(0,0);
-//    int N, K, repeticiones,construccion, num = 1;
-//    double parametrosSimulacion[5], p;
-//    string nombreRedEntrada, salidaExperimento;
-//    while( !archivoExperimentos.eof() ){
-//        salidaExperimento = "Salida_Experimento#" + to_string(num) + ".txt";
-//        ofstream salida( salidaExperimento.c_str() );
-//        for( int i = 0; i < 5; i++ ){
-//            archivoExperimentos >> parametrosSimulacion[i];
-//            if( (i != 0 && i != 2) ){
-//                if( !(parametrosSimulacion[i] <= 1.0) )
-//                    cout << "El valor vsc, rc o grc tomado de " << nombreExperimentos << " no se encuentra en el rango apropiado para hacer una comparacion con netlogoweb.org" << endl;
-//                if( i != 4 )
-//                    parametrosSimulacion[i] *= 0.10;
-//            }
-//        }
-//        archivoExperimentos >> repeticiones;
-//        archivoExperimentos >> construccion;
-//        archivoExperimentos.get();
-//        if( construccion == 3 ){
-//            archivoExperimentos >> nombreRedEntrada;
-//            ifstream archivo( nombreRedEntrada.c_str() );
-//            Grafo grafo1( archivo );
-//            grafo = grafo1;
-//        } else {
-//            archivoExperimentos >> N;
-//            if( construccion == 2 ){
-//                archivoExperimentos >> K;
-//                archivoExperimentos >> p;
-//                Grafo grafo1( N, K, p );
-//                grafo = grafo1;
-//            } else {
-//                archivoExperimentos >> p;
-//                Grafo grafo1( N, p );
-//                grafo = grafo1;
-//            }
-//        }
-//        for( int i = 0; i < repeticiones; i++ ){
-//            Grafo grafoCopia( grafo );
-//            Simulador simulador( grafoCopia );
-//            simulador.iniciarSimulacion( parametrosSimulacion[0], parametrosSimulacion[1], parametrosSimulacion[2], parametrosSimulacion[3], parametrosSimulacion[4] );
-//            simulador.simular();
-//            
-//            salida << "-----------------------------------------" << endl;
-//            salida << "Simulación " << i+1 << endl;
-//            salida << "-----------------------------------------" << endl;
-//            ifstream recibo( "salidaSimulacion.txt" );
-//            string temporal;
-//            while( !recibo.eof() ){
-//                while( recibo.peek() != '\n' && !recibo.eof() ){
-//                    recibo >> temporal;
-//                    salida << temporal << " ";
-//                }
-//                recibo.get();
-//                salida << endl;
-//            }
-//            recibo.close();
-//        }
-//        salida.close();
-//        remove( "salidaSimulacion.txt" );
-//        cout << "Se ha producido la " << salidaExperimento << endl;
-//        num++;
-//    }
+    cout << "Bienvenido al simulador de dispersión de virus." << endl;
+    cout << "Ingrese el nombre del archivo de experimentos: ";
+    string nombreExperimentos;
+    cin >> nombreExperimentos;
+    cout << endl;
+    ifstream archivoExperimentos( nombreExperimentos.c_str() );
+    if( archivoExperimentos.fail() ){
+        cout << "El archivo " << nombreExperimentos << " de extensión .txt no existe.";
+        return 0;
+    }
+    
+    Grafo grafo(0,0);
+    int N, K, repeticiones,construccion, num = 1;
+    double parametrosSimulacion[5], p;
+    string nombreRedEntrada, salidaExperimento;
+    while( !archivoExperimentos.eof() ){
+        salidaExperimento = "Salida_Experimento#" + to_string(num) + ".txt";
+        ofstream salida( salidaExperimento.c_str() );
+        for( int i = 0; i < 5; i++ ){
+            archivoExperimentos >> parametrosSimulacion[i];
+            if( (i != 0 && i != 2) ){
+                if( !(parametrosSimulacion[i] <= 1.0) )
+                    cout << "El valor vsc, rc o grc tomado de " << nombreExperimentos << " no se encuentra en el rango apropiado para hacer una comparacion con netlogoweb.org" << endl;
+                if( i != 4 )
+                    parametrosSimulacion[i] *= 0.10;
+            }
+        }
+        archivoExperimentos >> repeticiones;
+        archivoExperimentos >> construccion;
+        archivoExperimentos.get();
+        if( construccion == 3 ){
+            archivoExperimentos >> nombreRedEntrada;
+            ifstream archivo( nombreRedEntrada.c_str() );
+            Grafo grafo1( archivo );
+            grafo = grafo1;
+        } else {
+            archivoExperimentos >> N;
+            if( construccion == 2 ){
+                archivoExperimentos >> K;
+                archivoExperimentos >> p;
+                Grafo grafo1( N, K, p );
+                grafo = grafo1;
+            } else {
+                archivoExperimentos >> p;
+                Grafo grafo1( N, p );
+                grafo = grafo1;
+            }
+        }
+        for( int i = 0; i < repeticiones; i++ ){
+            Grafo grafoCopia( grafo );
+            Simulador simulador( grafoCopia );
+            simulador.iniciarSimulacion( parametrosSimulacion[0], parametrosSimulacion[1], parametrosSimulacion[2], parametrosSimulacion[3], parametrosSimulacion[4] );
+            simulador.simular();
+            
+            salida << "-----------------------------------------" << endl;
+            salida << "Simulación " << i+1 << endl;
+            salida << "-----------------------------------------" << endl;
+            ifstream recibo( "salidaSimulacion.txt" );
+            string temporal;
+            while( !recibo.eof() ){
+                while( recibo.peek() != '\n' && !recibo.eof() ){
+                    recibo >> temporal;
+                    salida << temporal << " ";
+                }
+                recibo.get();
+                salida << endl;
+            }
+            recibo.close();
+        }
+        salida.close();
+        remove( "salidaSimulacion.txt" );
+        cout << "Se ha producido la " << salidaExperimento << endl;
+        num++;
+    }
     
     return 0;
 }
